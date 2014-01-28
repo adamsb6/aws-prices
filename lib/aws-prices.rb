2f961763e29e1ff88c29f648c5d7c967cf4054fe
James Harries <vaicine@vaicine.com>
2014-01-28 09:52:06 +0000

    Updating pricing uri

--- lib/aws-prices.rb
+++ lib/aws-prices.rb
@@ -3,7 +3,7 @@ require 'net/http'
 
 class AWSPrices
     def awsnow_price(region, instance_type, os)
-      uri = URI("http://api.awsnow.info/ec2/#{os}/?region=#{region}&instance=#{instance_type}&tenancy=default&option=demand")
+      uri = URI("http://api.cloudomix.com/amazon/ec2/#{os}/?region=#{region}&instance=#{instance_type}&tenancy=default&option=demand")
       http = Net::HTTP.new uri.host, uri.port
       http.open_timeout = 60
       http.read_timeout = 60
